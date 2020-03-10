452687079e5859124a14d0d996746a50ef07f4b5
Andrew <atksmith88@gmail.com>
2020-03-09 20:12:13 -0500

    Iniialized repository, added lists.py file, started on README.ipynb.

---
+++ lists.py
@@ -0,0 +1,42 @@
+buffet = ['Star Buffet', 'Golden Corral', 'Sirloin Stockade', "Cici's"]
+
+breakfast = ["Denny's", 'IHOP', 'Kerbey Lane Cafe', 'Waffle House']
+
+mexican = ['Morelia', 'Freebirds', "Chuy's", 'Mesa Rosa',
+           'Mas Fajitas', "Rosa's Cafe & Tortilla Factory"]
+
+bbq = ['Bill Miller', "Rudy's", "Smokey Mo's"]
+
+american = ["Applebee's", 'Cotton Patch Cafe', "BJ's", 'Kerbey Lane Cafe']
+
+steakhouse = ["Logan's", 'Texas Roadhouse', 'Longhorn', 'Outback',
+              'CR Surf & Turf']
+
+asian = ['Star Buffet', "Mama Fu's", 'Dr. Wok']
+
+diner = ["Denny's", "Jim's", 'Kerbey Lane Cafe']
+
+seafood = ['Red Lobster', 'Catfish Parlour', 'Spinghill Catfish',
+           'CR Surf & Turf']
+
+chicken = ['Pluckers', "Cane's", "Bush's", 'Golden Chick']
+
+bakery_cafe = ['Cotton Patch Cafe', 'Texan Cafe & Pie Shop',
+               'Kerbey Lane Cafe']
+
+italian = ['Olive Garden', "BJ's", 'Macaroni Grill', 'Serranos']
+
+pizza = ["BJ's", "Cici's", 'Mangia']
+
+burgers = ['Red Robin', "Arby's", "BJ's", 'Kerbey Lane Cafe']
+
+pasta = ['Olive Garden', "BJ's", 'Noodle House', 'Macaroni Grill']
+
+dinner_and_a_movie = ['Flix Brewhouse', 'Alamo Drafthouse',
+                      'Moviehouse and Eatery']
+
+main = {20: 'Cheesecake Factory', 19: buffet, 18: breakfast, 17: mexican,
+        16: bbq, 15: american, 14: steakhouse, 13: asian,
+        12: seafood, 11: diner, 10: 'Something New', 9: chicken,
+        8: bakery_cafe, 7: italian, 6: pizza, 5: burgers, 4: pasta,
+        3: dinner_and_a_movie, 2: 'Random', 1: 'Star Buffet'}
